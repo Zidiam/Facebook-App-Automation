d44db0173412102a5de8b8989552862335166d6e
Zidiam <33102169+Zidiam@users.noreply.github.com>
2021-12-03 02:32:50 -0500

    Clean Code

--- Android_Code/tests/post.test.js
+++ Android_Code/tests/post.test.js
@@ -6,14 +6,15 @@ const wdio = require("webdriverio");
 jest.setTimeout(200000);
 
 //Declare Global Variables
-let CORRECT_EMAIL;
-let CORRECT_PASS;
+let CORRECT_EMAIL1;
+let CORRECT_PASS1;
+let CORRECT_EMAIL2;
+let CORRECT_PASS2;
 let client;
 let LOADING;
 let EMAIL_TXT_FIELD;
 let PASSWORD_TXT_FIELD;
 let LOGIN_BTN;
-let POPUP;
 
 //This code is specific to appium to connect to device
 const opts = {
@@ -38,29 +39,32 @@ async function Loading(){
 async function LogIn(){
     //declare element selector values
     const SKIP = '~Skip';
+    const SKIP_CONFIRM = 'text("SKIP").className("android.widget.Button")';
     const PROFILE = '~Go to profile';
 
-    await client.$(EMAIL_TXT_FIELD).setValue(CORRECT_EMAIL);
-    await client.$(PASSWORD_TXT_FIELD).setValue(CORRECT_PASS);
+    await client.$(EMAIL_TXT_FIELD).setValue(CORRECT_EMAIL1);
+    await client.$(PASSWORD_TXT_FIELD).setValue(CORRECT_PASS1);
     await client.$(LOGIN_BTN).click();
 
     await Loading();
 
-    //New users have a skip popup
-    if(await client.$(SKIP).isDisplayed()){
+    while(await client.$(SKIP).isDisplayed()){
         await client.$(SKIP).click();
-        await Loading();
-    };
+        if(await client.$(`android=${SKIP_CONFIRM}`).isDisplayed()){
+            await client.$(`android=${SKIP_CONFIRM}`).click();
+            await Loading();
+        };
+    }
 
-    const confirmed = await client.$(PROFILE).isDisplayed();
+    await client.$(PROFILE).isDisplayed();
 };
 
 
 //This runs before any of the tests run
-beforeAll(()=>{
+beforeAll( async()=>{
     //ENTER YOUR EMAIL AND PASSWORD
-    CORRECT_EMAIL = 'fekvtxc_chengwitz_1638509068@tfbnw.net';
-    CORRECT_PASS = 'ogct5b65vc1';
+    CORRECT_EMAIL1 = 'fekvtxc_chengwitz_1638509068@tfbnw.net';
+    CORRECT_PASS1 = 'ogct5b65vc1';
 
     //Accessibility ID
     EMAIL_TXT_FIELD = '~Username';
@@ -69,17 +73,13 @@ beforeAll(()=>{
 
     //Xpath
     LOADING = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ProgressBar';
-    POPUP = '/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.TextView';
-});
-
-//This runs before each test
-beforeEach( async() => {
+    
     client = await wdio.remote(opts);
     await LogIn();
 });
 
 //This runs after each test
-afterEach( async() => {
+afterAll( async() => {
     await client.deleteSession();
 });
 
@@ -92,17 +92,22 @@ test('Connected', async() =>{
 test('Successfull Post', async() =>{
     //declare element selector values
     await Loading();
-    const TEXT_FIELD = '//android.view.ViewGroup[@content-desc="Make a post on Facebook"]';
-    const INPUT_FIELD = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.EditText';
+    const TEXT_FIELD = '~Make a post on Facebook';
+    const INPUT_FIELD = '~Write something…';
     const POST_BTN = '~POST';
-    const CONFIRMED_TEXT = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[3]/android.view.ViewGroup[2]';
-
+    const POST_OPTIONS = '~Post Menu';
+    const EDIT_POST = 'text("Edit post").className("android.widget.TextView")';
+    
     await client.$(TEXT_FIELD).click();
-    await client.$(INPUT_FIELD).setValue('Test Post' + Math.floor(Math.random() * 10));
+    await client.$(INPUT_FIELD).click();
+    const post_text = 'Test Post' + Math.floor(Math.random() * 10);
+    await client.$(":focused").setValue(post_text);
     await client.$(POST_BTN).click();
-
-    await Loading();
-
-    const confirmed = await client.$(CONFIRMED_TEXT).isDisplayed();
+    await client.pause(1000);
+    await client.$(POST_OPTIONS).click();
+    await client.$(`android=${EDIT_POST}`).click();
+    
+    const test_post = 'text("' + post_text + '").className("android.widget.EditText")';
+    const confirmed = await client.$(`android=${test_post}`).isDisplayed();
     expect(confirmed).toBe(true);
 });
